8ebe3378d7b5ca72172dc6f4be68ef9f853f7bd3
sharaalt <103602431+sharaalt@users.noreply.github.com>
2022-04-13 15:19:35 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+       *farm|farms
+              name:Farm|farms
+              desc:</q>Farm some food and sell it to make some cash<q>
+              icon:https://pipe.miroware.io/61f31a16b1fc4b0dc6b59beb/Game%20stuff/Basic_Farm.png
+              cost:400 dollars
+              on tick:yield 30 dollars
+              req:300 dollars:earned
